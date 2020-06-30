0f214a0c552bfb2e3822f5cd0369f7cd52c11686
孙飞 <sunfei@sunfeideMacBook-Pro-2.local>
2020-06-30 14:38:49 +0800

    remove etcd/

--- etcd/etcd_http.go
+++ etcd/etcd_http.go
@@ -1,31 +1,31 @@
 package etcd
 
-import (
-	"time"
+// import (
+// 	"time"
 
-	"github.com/imdevlab/g"
-	"github.com/valyala/fasthttp"
-)
+// 	"github.com/imdevlab/g"
+// 	"github.com/valyala/fasthttp"
+// )
 
-var HTTP = &Http{}
+// var HTTP = &Http{}
 
-type Http struct{}
+// type Http struct{}
 
-func (h *Http) Reqeust(app string, path string, method string, args *fasthttp.Args) (*fasthttp.Response, error) {
-	req := &fasthttp.Request{}
-	req.Header.SetMethod(method)
+// func (h *Http) Reqeust(app string, path string, method string, args *fasthttp.Args) (*fasthttp.Response, error) {
+// 	req := &fasthttp.Request{}
+// 	req.Header.SetMethod(method)
 
-	s := GetServer(app)
-	url := "http://" + s.IP + path
-	switch method {
-	case "GET":
-		url = url + "?" + args.String()
-	default:
-		args.WriteTo(req.BodyWriter())
-	}
-	req.SetRequestURI(url)
+// 	s := GetServer(app)
+// 	url := "http://" + s.IP + path
+// 	switch method {
+// 	case "GET":
+// 		url = url + "?" + args.String()
+// 	default:
+// 		args.WriteTo(req.BodyWriter())
+// 	}
+// 	req.SetRequestURI(url)
 
-	resp := &fasthttp.Response{}
-	err := g.Cli.DoTimeout(req, resp, g.HTTP_REQ_TIMEOUT*time.Second)
-	return resp, err
-}
+// 	resp := &fasthttp.Response{}
+// 	err := g.Cli.DoTimeout(req, resp, g.HTTP_REQ_TIMEOUT*time.Second)
+// 	return resp, err
+// }
